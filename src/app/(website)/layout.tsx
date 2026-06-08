df227e05d81ced995c8f14e0426562154a8486f2
ADMIN <surendirandeveloper@gmail.com>
2026-06-08 23:25:02 +0400

    :about

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -1,17 +1,26 @@
-import Header from "../components/header"
+import Header from "../components/header";
 import Footer from "../components/Footer";
 
+export const dynamic = "force-dynamic";
+
 async function getNavbar() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/navbar`,
-    {
-      next: { revalidate: 3600 },
-    }
-  );
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/v1/navbar`,
+      {
+        cache: "no-store",
+      }
+    );
 
-  const data = await res.json();
+    if (!res.ok) return [];
 
-  return data.success ? data.data : [];
+    const data = await res.json();
+
+    return data.success ? data.data : [];
+  } catch (err) {
+    console.log("Navbar fetch failed:", err);
+    return [];
+  }
 }
 
 export default async function Layout({
@@ -19,13 +28,13 @@ export default async function Layout({
 }: {
   children: React.ReactNode;
 }) {
-   const navLinks = await getNavbar();
+  const navLinks = await getNavbar();
 
   return (
     <>
-      <Header navLinks={navLinks}/>
+      <Header navLinks={navLinks} />
       {children}
-      <Footer navLinks={navLinks}/>
+      <Footer navLinks={navLinks} />
     </>
   );
 }
